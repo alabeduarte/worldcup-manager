ae66a3dbaccf4e834efea2447ccdee79c96602ee
Alabê Duarte <alabeduarte@gmail.com>
2013-12-15 11:10:13 -0300

    Changed url format from '/players/:countryId' to '/countries/:countryId/players'

--- www/js/home.controller.js
+++ www/js/home.controller.js
@@ -1,5 +1,5 @@
 angular.module('worldcup-manager')
 .controller('Home', ['$scope', 'WorldSoccerAPI', function ($scope, WorldSoccerAPI) {
   WorldSoccerAPI.all('countries', function (countries) { $scope.countries = countries; });
-  WorldSoccerAPI.all('players/brazil', function (players) { $scope.players = players; });
+  WorldSoccerAPI.all('countries/bra/players', function (players) { $scope.players = players; });
 }]);
