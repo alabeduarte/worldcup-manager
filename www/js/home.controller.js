872ba299e6302365485f480a4704a8cbdaa82927
Alabê Duarte <alabeduarte@gmail.com>
2013-12-14 22:44:36 -0300

    Fixed async request

--- www/js/home.controller.js
+++ www/js/home.controller.js
@@ -1,4 +1,4 @@
 angular.module('worldcup-manager')
 .controller('Home', ['$scope', 'Player', function ($scope, Player) {
-  $scope.players = Player.all();
+  Player.all(function (players) { $scope.players = players; });
 }]);
--- www/js/player.factory.js
+++ www/js/player.factory.js
@@ -1,11 +1,15 @@
 angular.module('worldcup-manager').factory('Player',
-['$http', 'localStorageService', function ($http, localStorage) {
-  return { all: function () {
-    return localStorage.get('players') || $http.get(
-    'http://secure-everglades-2636.herokuapp.com/players/brazil', { transformResponse: function (data) {
-      localStorage.add('players', data);
-      return data;
-    }});
-    }
-  };
+  ['$http', 'localStorageService', function ($http, localStorage) {
+    return {
+      all: function (success) {
+        if (!localStorage.get('players')) {
+          $http.get('http://worldsoccer-api.herokuapp.com/players/brazil', {
+            transformResponse: function (data) {
+              localStorage.add('players', data);
+              success(data);
+            }
+          });
+        } else success(localStorage.get('players'));
+      }
+    };
 }]);
