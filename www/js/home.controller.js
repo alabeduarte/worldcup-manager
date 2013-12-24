3650bec567ef59d18015b52388a236db8cfaa568
Alabê Duarte <alabeduarte@gmail.com>
2013-12-24 18:45:58 -0300

    WIP

--- www/js/home.controller.js
+++ www/js/home.controller.js
@@ -1,5 +1,11 @@
 angular.module('worldcup-manager')
 .controller('Home', ['$scope', 'WorldSoccerAPI', function ($scope, WorldSoccerAPI) {
-  WorldSoccerAPI.all('countries', function (countries) { $scope.countries = countries; });
-  WorldSoccerAPI.all('countries/bra/players', function (players) { $scope.players = players; });
+
+  WorldSoccerAPI.all('countries', function (countries) {
+    $scope.countries = countries;
+  });
+
+  WorldSoccerAPI.all('countries/bra/players', function (players) {
+    $scope.players = players;
+  });
 }]);
