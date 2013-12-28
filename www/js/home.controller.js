1f8239f7e626d6f0acf53beb16275ea851acf48a
Alabê Duarte <alabeduarte@gmail.com>
2013-12-28 18:52:32 -0300

    Revert "WIP"
    This reverts commit 3650bec567ef59d18015b52388a236db8cfaa568.

--- www/js/home.controller.js
+++ www/js/home.controller.js
@@ -1,11 +1,5 @@
 angular.module('worldcup-manager')
 .controller('Home', ['$scope', 'WorldSoccerAPI', function ($scope, WorldSoccerAPI) {
-
-  WorldSoccerAPI.all('countries', function (countries) {
-    $scope.countries = countries;
-  });
-
-  WorldSoccerAPI.all('countries/bra/players', function (players) {
-    $scope.players = players;
-  });
+  WorldSoccerAPI.all('countries', function (countries) { $scope.countries = countries; });
+  WorldSoccerAPI.all('countries/bra/players', function (players) { $scope.players = players; });
 }]);
