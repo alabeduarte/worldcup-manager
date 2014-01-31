bb75e930df4f01a1ddfd30c8298ac56f9c6ae092
Alabê Duarte <alabeduarte@gmail.com>
2014-01-31 19:39:43 -0300

    Making test pass

--- www/spec/formations.factory.spec.js
+++ www/spec/formations.factory.spec.js
@@ -1,8 +1,11 @@
 describe('Formations Factory', function() {
-  var Formations;
+  var factory;
 
   beforeEach(function () {
     angular.module('worldcup-manager');
+    inject(function (Formations) {
+      factory = Formations;
+    });
   });
 
   describe('Listing Formations', function() {
@@ -10,17 +13,49 @@ describe('Formations Factory', function() {
       {
         back  : 4,
         middle: 4,
-        foward: 2,
-        scheme: '4-4-2'
+        foward: 2
+      },
+      {
+        back  : 4,
+        middle: 5,
+        foward: 1
+      },
+      {
+        back  : 4,
+        middle: 3,
+        foward: 3
+      },
+      {
+        back  : 3,
+        middle: 4,
+        foward: 3
+      },
+      {
+        back  : 3,
+        middle: 5,
+        foward: 2
+      },
+      {
+        back  : 5,
+        middle: 3,
+        foward: 2
+      },
+      {
+        back  : 5,
+        middle: 4,
+        foward: 1
       }
     ];
 
-    it('should list formation 4-4-2', function() {
-      inject(function (Formations) {
-        var formations = Formations.list();
-        expect(expectedFormations).toEqual(formations);
-      });
+    it('should list all formations', function() {
+      var formations = factory.list();
+      expect(expectedFormations).toEqual(formations);
     });
 
   });
+
+  describe('Listing Formations', function() {
+
+  });
+
 });
