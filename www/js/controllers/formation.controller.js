4504a1f5d0cb1fd9692a36224d8ace85eafa52f0
Alabê Duarte <alabeduarte@gmail.com>
2014-02-09 15:21:25 -0300

    Added click event to formation

--- www/js/controllers/formation.controller.js
+++ www/js/controllers/formation.controller.js
@@ -2,15 +2,17 @@ angular.module('worldcup-manager')
 .controller('FormationController', ['$scope', '$routeParams', 'WorldSoccerAPI', 'FormationList', '_', function ($scope, $routeParams, WorldSoccerAPI, FormationList, _) {
   var countryId = $routeParams.countryId;
   $scope.formations =  FormationList.list();
-
   $scope.selectedFormation = $scope.formations[0];
-  $scope.getNumber = function (number) { return new Array(number); }
 
+  $scope.getNumber = function (number) { return new Array(number); }
   $scope.selectFormation = function (formation) {
     var formations = $scope.formations;
     var index = _.indexOf(formations, formation);
     $scope.selectedFormation = formations[index];
   };
+  $scope.wantToSelect = function () {
+    $scope.selectedFormation.wantToSelect = true;
+  }
 
   WorldSoccerAPI.get('/countries/' + countryId + '/players', function (players) {
     $scope.players = players;
