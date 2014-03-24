e5ab71a89d63d126e21a39f2a794ec808e45bc64
Alabê Duarte <alabeduarte@gmail.com>
2014-03-23 22:04:31 -0300

    Checking undefined variable when cordova is disabled

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,12 @@
 var app = {
   initialize: function() {
+    var isCordovaDefined = typeof cordova != 'undefined';
     var fixiOSStatusBarIssue = function () {
-      if (cordova) {
+      if (isCordovaDefined) {
         if (parseFloat(window.device.version) === 7.0) document.body.style.marginTop = "20px";
       }
     };
-    var bootStrapApp = function () {
+    var bootstrapApp = function () {
       fixiOSStatusBarIssue();
       angular.element(document).ready(function () {
         console.log('angular ready!');
@@ -13,7 +14,7 @@ var app = {
       });
     };
 
-    if (cordova) document.addEventListener('deviceready', bootStrapApp, false);
-    else bootStrapApp();
+    if (isCordovaDefined) document.addEventListener('deviceready', bootstrapApp, false);
+    else bootstrapApp();
   }
 };
